e160123dc100c98a2c0fd11976f14e5d73977338
Evan Gui <evan.gui@shopee.com>
2020-03-06 14:48:38 +0800

    Add cosine similarity

--- hnsw.go
+++ hnsw.go
@@ -10,6 +10,7 @@ package hnswgo
 // void setEf(HNSW index, int ef);
 import "C"
 import (
+	"math"
 	"unsafe"
 )
 
@@ -17,6 +18,7 @@ type HNSW struct {
 	index     C.HNSW
 	spaceType string
 	dim       int
+	normalize bool
 }
 
 func New(dim, M, efConstruction, randSeed int, maxElements uint32, spaceType string) *HNSW {
@@ -24,6 +26,9 @@ func New(dim, M, efConstruction, randSeed int, maxElements uint32, spaceType str
 	hnsw.dim = dim
 	hnsw.spaceType = spaceType
 	if spaceType == "ip" {
+		hnsw.index = C.initHNSW(C.int(dim), C.ulong(maxElements), C.int(M), C.int(efConstruction), C.int(randSeed), C.char('i'))
+	} else if spaceType == "cosine" {
+		hnsw.normalize = true
 		hnsw.index = C.initHNSW(C.int(dim), C.ulong(maxElements), C.int(M), C.int(efConstruction), C.int(randSeed), C.char('i'))
 	} else {
 		hnsw.index = C.initHNSW(C.int(dim), C.ulong(maxElements), C.int(M), C.int(efConstruction), C.int(randSeed), C.char('l'))
@@ -39,6 +44,9 @@ func Load(location string, dim int, spaceType string) *HNSW {
 	pLocation := C.CString(location)
 	if spaceType == "ip" {
 		hnsw.index = C.loadHNSW(pLocation, C.int(dim), C.char('i'))
+	} else if spaceType == "cosine" {
+		hnsw.normalize = true
+		hnsw.index = C.loadHNSW(pLocation, C.int(dim), C.char('i'))
 	} else {
 		hnsw.index = C.loadHNSW(pLocation, C.int(dim), C.char('l'))
 	}
@@ -52,12 +60,30 @@ func (h *HNSW) Save(location string) {
 	C.free(unsafe.Pointer(pLocation))
 }
 
+func normalizeVector(vector []float32) []float32 {
+	var norm float32
+	for i := 0; i < len(vector); i++ {
+		norm += vector[i] * vector[i]
+	}
+	norm = 1.0 / (float32(math.Sqrt(float64(norm))) + 1e-15)
+	for i := 0; i < len(vector); i++ {
+		vector[i] = vector[i] * norm
+	}
+	return vector
+}
+
 func (h *HNSW) AddPoint(vector []float32, label uint32) {
+	if h.normalize {
+		vector = normalizeVector(vector)
+	}
 	C.addPoint(h.index, (*C.float)(unsafe.Pointer(&vector[0])), C.ulong(label))
 }
 
 func (h *HNSW) SearchKNN(vector []float32, N int) []uint32 {
 	result := make([]C.ulong, N, N)
+	if h.normalize {
+		vector = normalizeVector(vector)
+	}
 	numResult := int(C.searchKnn(h.index, (*C.float)(unsafe.Pointer(&vector[0])), C.int(N), &result[0]))
 	labels := make([]uint32, N)
 	for i := 0; i < numResult; i++ {
