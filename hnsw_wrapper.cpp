3ac61721ee3d58656cdb36b3b8855ebaf30c10ae
ryan-awad <ryanawad@yahoo.com>
2024-09-15 09:57:31 -0400

    Added tests and error handling for C++ wrapper

--- hnsw_wrapper.cpp
+++ hnsw_wrapper.cpp
@@ -1,5 +1,27 @@
-#include "hnswlib/hnswlib.h"
+#include "lib/hnswlib/hnswlib.h"
 #include "hnsw_wrapper.h"
+
+static thread_local std::string lastErrorMsg; // stores the last error message
+
+/**
+ * Returns the last error message if there exists one. If no error message exists, a null pointer will be returned.
+ * 
+ * @return last error message or a null pointer
+ */
+char* peekLastErrorMsg() {
+    return lastErrorMsg.empty() ? nullptr : strdup(lastErrorMsg.c_str());
+}
+
+/**
+ * Returns the last error message AND clears it, if there exists one. If no error message exists, a null pointer will be returned.
+ * 
+ * @return last error message or a null pointer
+ */
+char* getLastErrorMsg() {
+    char *err = peekLastErrorMsg();
+    lastErrorMsg.clear();
+    return err;
+}
 
 /**
  * Instantiates and returns an HNSW index.
@@ -9,21 +31,29 @@
  * @param m:                `m` parameter in the HNSW algorithm
  * @param efConstruction:   `efConstruction` parameter in the HNSW algorithm
  * @param randSeed:         random seed
- * @param spaceType:        similarity metric to use in the index
+ * @param spaceType:        similarity metric to use in the index ("l" = L2, "i" = IP, "c" = cosine). (default: "l")
  * 
  * @return                  instance of a HNSW index
  */
 HNSW initHNSW(int dim, unsigned long int maxElements, int m, int efConstruction, int randSeed, char spaceType) {
-    hnswlib::SpaceInterface<float> *vectorSpace;
-    if (spaceType == 'i') { // inner product
-        vectorSpace = new hnswlib::InnerProductSpace(dim);
+    try {
+        hnswlib::SpaceInterface<float> *vectorSpace;
+        if (spaceType == 'i') { // inner product
+            vectorSpace = new hnswlib::InnerProductSpace(dim);
+        }
+        else if (spaceType == 'c') { // cosine (cosine is the same as IP when all vectors are normalized)
+            vectorSpace = new hnswlib::InnerProductSpace(dim);
+        } else { // default: L2
+            vectorSpace = new hnswlib::L2Space(dim);
+        }
+        return new hnswlib::HierarchicalNSW<float>(vectorSpace, maxElements, m, efConstruction, randSeed); // instantiate the hnsw index
+    } catch (const std::runtime_error e) {
+        lastErrorMsg = std::string(e.what());
+        return nullptr;
+    } catch(const std::exception e) {
+        lastErrorMsg = std::string(e.what());
+        return nullptr;
     }
-    else if (spaceType == 'c') { // cosine (cosine is the same as IP when all vectors are normalized)
-        vectorSpace = new hnswlib::InnerProductSpace(dim);
-    } else { // default: L2
-        vectorSpace = new hnswlib::L2Space(dim);
-    }
-    return new hnswlib::HierarchicalNSW<float>(vectorSpace, maxElements, m, efConstruction, randSeed); // instantiate the hnsw index
 }
 
 /**
@@ -43,7 +73,14 @@ void freeHNSW(HNSW hnswIndex) {
  * @param label:        the vector's label
  */
 void insertVector(HNSW hnswIndex, float *vector, unsigned long int label) {
-    ((hnswlib::HierarchicalNSW<float>*) hnswIndex)->addPoint(vector, label);
+    // ** I believe a vector is overwritten when you insert another vector with the same label **
+    try {
+        ((hnswlib::HierarchicalNSW<float>*) hnswIndex)->addPoint(vector, label);
+    } catch(const std::runtime_error e) {
+        lastErrorMsg = std::string(e.what());
+    } catch (const std::exception e) {
+        lastErrorMsg = std::string(e.what());
+    }
 }
 
 /**
@@ -55,25 +92,29 @@ void insertVector(HNSW hnswIndex, float *vector, unsigned long int label) {
  * @param labels:       a dynamic array which will receive the labels of the k-nearest neighbors
  * @param distances:    a dynamic array which will receive the distances of the k-nearest neighbors from the query vector
  * 
- * @return              the number of nearest neighbors found (num of nn <= k since it's possible for k > num of vectors in the index)
+ * @return              the number of nearest neighbors found (num of nn <= k since it's possible for k > num of vectors in the index). If an error occured, it will return -1.
  */
 int searchKNN(HNSW hnswIndex, float *vector, int k, unsigned long int *labels, float *distances) {
     std::priority_queue<std::pair<float, hnswlib::labeltype>> searchResults;
     try {
         searchResults = ((hnswlib::HierarchicalNSW<float>*) hnswIndex)->searchKnn(vector, k);
-    } catch (const std::exception e) { 
-        return 0; // get better error visibility
-    }
 
-    int n = searchResults.size();
-    std::pair<float, hnswlib::labeltype> pair;
-    for (int i = n - 1; i >= 0; i--) {
-        pair = searchResults.top();
-        distances[i] = pair.first;
-        labels[i] = pair.second;
-        searchResults.pop();
+        int n = searchResults.size();
+        std::pair<float, hnswlib::labeltype> pair;
+        for (int i = n - 1; i >= 0; i--) {
+            pair = searchResults.top();
+            distances[i] = pair.first;
+            labels[i] = pair.second;
+            searchResults.pop();
+        }
+        return n;
+    } catch (const std::runtime_error e) {
+        lastErrorMsg = std::string(e.what());
+        return -1;
+    } catch (const std::exception e) {
+        lastErrorMsg = std::string(e.what());
+        return -1;
     }
-    return n;
 }
 
 /**
--- hnsw_wrapper.h
+++ hnsw_wrapper.h
@@ -2,6 +2,8 @@
 extern "C" {
 #endif
     typedef void* HNSW;
+    char* peekLastErrorMsg();
+    char* getLastErrorMsg();
     HNSW initHNSW(int dim, unsigned long int maxElements, int m, int efConstruction, int randSeed, char simMetric);
     void freeHNSW(HNSW hnswIndex);
     void insertVector(HNSW hnswIndex, float *vector, unsigned long int label);
